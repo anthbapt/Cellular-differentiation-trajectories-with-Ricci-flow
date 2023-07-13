206cbd258bd94356889f1e684d2e7051cd4f04af
Anthony Baptista <35119963+anthbapt@users.noreply.github.com>
2023-07-13 16:57:09 +0100

    Update curvature_and_flow.r

--- curvature_and_flow.r
+++ curvature_and_flow.r
@@ -4,13 +4,16 @@
 #####int.red= n x n adjacency matrix describing how the proteins corresponding to the genes interact:
 #####
 #####first compute the degree of each gene and the inverse
+
 apply(int.red,1,sum)->deg
 1/deg->D
+
 #####generate a list to index the neighbours of each gene
 lapply(1:nrow(int.red), function(i){
   print(i)
   return(which(int.red[i,]>0))
 })->neighbs
+
 #####define the Forman Ricci curvature using node weights W_i=1/deg(i); w_ij=1/xi*xj
 F_Ricci2<-function(i,j,a){###input a is a weighted matrix where aij=xi*xj is (i,j) \in E, 0 else
   if(int.red[i,j]==0){return(NA)}else{
@@ -19,10 +22,12 @@ F_Ricci2<-function(i,j,a){###input a is a weighted matrix where aij=xi*xj is (i,
     D[i]+D[j]-(1/a[i,j])*(D[i]*sum((q)^(-1/2))+D[j]*sum((q2)^(-1/2)))->r
     return(r)
   }}
+
 #####another function to output Ricci curvatures for all edges incident on a given node i
 F_Ricci_row2<-function(i,a){
   return(mapply(function(j){return(F_Ricci2(i,j,a))},neighbs[[i]]))
 }
+
 #####computation of scaler Ricci curvature at each nodes, for each column of x
 mcmapply(function(i){
   print(i)
@@ -37,6 +42,7 @@ mcmapply(function(i){
     return((sum(F_Ricci_mat[[k]])))
   },1:length(F_Ricci_mat))->F_n#
   return(F_n)},1:ncol(x),mc.cores = 8)->F_G_full_vec2
+
 ######compute stationary distribution for each column of x
 mcmapply(function(i){
   print(i)
@@ -48,9 +54,12 @@ mcmapply(function(i){
   a/b->c##stoch mat
   b/sum(b)->mu
   return(mu)},1:ncol(x),mc.cores = 8)->mu_full_vec
+
 #########compute total Ricci curvature for each column of x
 mu_full_vec*D*F_G_full_vec2->F_G_pre_sum2
 apply(F_G_pre_sum2,2,sum)->F_G2###this gives T values of total Ricci curvature, one for each column of x
+
+
 #########compare with entropy rate
 #########compute nodal entropies
 mapply(function(i){
@@ -63,10 +72,14 @@ mapply(function(i){
   a/b->c##stoch mat
   apply(c,1,ent)->e
   return(e)},test)->ent_full_vec
+
 ##############compute signalling entropy
 apply(mu_full_vec*ent_full_vec,2,sum)->SR
+
 #######
 plot(SR,F_G2)
+
+
 #######iterate Ricci Flow
 #######define starting and ending states of Ricci flow and corresponding edge curvatures:
 ########worked example for Chu et al., time course on average gene expression at 6 time points during differentiation
@@ -75,21 +88,25 @@ v<-x[,1]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_1
+
 ########
 mcmapply(function(j){
   print(j)
   return(F_Ricci_row2(j,a_1))
 },1:ncol(int.red),mc.cores = 10)->F_Ricci_mat_1
-#########end sate
+
+#########end state
 v<-x[,ncol(x)]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_2
+
 ##########
 mcmapply(function(j){
   print(j)
   return(F_Ricci_row2(j,a_2))
 },1:ncol(int.red),mc.cores = 10)->F_Ricci_mat_2
+
 ###########Pick largest Delta t (here called eps) s.t. keep positive weights post first iteration
 seq(0.01,0.1,0.01)->eps_seq
 mapply(function(k){
@@ -100,38 +117,45 @@ mapply(function(k){
   ##############itterate weights one step
   R_flow_c(a_1,F_Ricci_mat_1,F_Ricci_mat_2)->a_t
   return(length(which(unlist(a_t)<0))/length(unlist(a_t)))},1:length(eps_seq))->prop_zero
-cbind(eps_seq,prop_zero)##this will output the optimal eps, here 0.06
+cbind(eps_seq,prop_zero) ##this will output the optimal eps, here 0.06
+
 ####################
 eps<-0.06
 ####################
+
 ############define ricci flow
 R_flow_c<-function(a_t,F_Ricci_mat_t,F_Ricci_mat_2){return(
   mapply(function(i){
     mapply(function(j){
       return((1/a_t[i,neighbs[[i]][j]])+eps*(F_Ricci_mat_t[[i]][j]-F_Ricci_mat_2[[i]][j])*(1/a_t[i,neighbs[[i]][j]]))},1:length(neighbs[[i]]))},1:ncol(a_t)))}
+
 ############
 #define the 4 intermediate states
 v<-x[,2]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_1_1
+
 ###
 v<-x[,3]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_1_2
+
 ###
 v<-x[,4]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_1_3
+
 ###
 v<-x[,4]
 outer(v,v)->a
 a[which(int.red==0)]<-0
 a->a_1_4
-####
+
 #########set number of iterations to run and empty vectors to populate
+
 K<-150
 fts<-list()
 ent_iters<-list()
@@ -142,9 +166,11 @@ cors_a1_2<-matrix(0,K,1)
 cors_a1_3<-matrix(0,K,1)
 cors_a1_4<-matrix(0,K,1)
 cors_a2<-matrix(0,K,1)
+
 ##########initiate starting state
 a_t<-a_1
 F_Ricci_mat_t<-F_Ricci_mat_1
+
 #############################run K iterations of the flow
 for(l in 1:K){
   print(l)
@@ -176,19 +202,23 @@ for(l in 1:K){
   },1:ncol(int.red),mc.cores = 8)->F_Ricci_mat_t
   mcmapply(function(k){return((sum(F_Ricci_mat_t[[k]])))},1:length(F_Ricci_mat_t),mc.cores = 8)->fts[[l]]
 }
+
 ################################copmute scalar curvatures and signalling entropies for eack of K iterations
 unlist(lapply(1:length(fts),function(x){return(sum(mu_iters[[x]]*D*fts[[x]]))}))->ft_as
 c(F_G2[2],ft_as)->ft_as
 # unlist(lapply(1:length(fts),function(x){return(sum(mu_iters[[x]]*ent_iters[[x]]))}))->SR_as
 # c(SR[2],SR_as)->SR_as
+
 ####################################confirm convergence
 plot(ft_as,type="l",lwd=2,xlab="k",
      col="blue",ylab="Total Forman-Ricci Curvature");abline(h=F_G2[c(1,ncol(x))],col="red")
 plot(SR_as,type="l",lwd=2,ylim=c(6.707,6.717),xlab="k",
      col="blue",ylab="Signalling Entropy");abline(h=SR[c(1,ncol(x))],col="red")
+
 #####################################
 cbind(cors_a1,cors_a1_1,cors_a1_2,cors_a1_3,cors_a1_3,cors_a1_4,cors_a2)->cors_all
 apply(cors_all_a,2,function(x){which(x==min(x))})###display first passing iteration for each intermediate data point
+
 #############Euclidean trajectory null model
 ####convert to weighted graph
 to_edge<-function(x){
@@ -198,13 +228,16 @@ to_edge<-function(x){
     return(a[which(int.red!=0)])
   }, 1:ncol(x))->ed
   return(ed)}
+
 ####
 to_edge(x)->x2
 ####
+
 x2[,1]->x_1
 x2[,ncol(x)]->x_n
 ####make straight line trajectory
 y<-function(t){return(x_1+t*(x_n-x_1)/150)}
+
 ####compute distance between 6 intermediate time points
 mapply(function(i){
   print(i)
@@ -220,8 +253,10 @@ t(ds)->ds
 apply(ds,2,function(x){which(x==min(x))})
 plot(apply(ds,2,function(x){which(x==min(x))}),c(0,12,24,36,72,96))
 cor.test(apply(ds,2,function(x){which(x==min(x))})[-c(1,6)],c(0,12,24,36,72,96)[-c(1,6)])
+
 ################
 times<-c(0,12,24,36,72,96)###true data point differentiation times
+
 ###RF, closest pass of trajectory plotted against true diff time
 cor.test(times[-c(1,6)],apply(cors_all_a,2,function(x){which(x==min(x))})[-c(1,6)])->ct
 plot(c(apply(cors_all_a,2,function(x){which(x==min(x))})[1:5],150),times,pch=16,col=
@@ -229,6 +264,7 @@ plot(c(apply(cors_all_a,2,function(x){which(x==min(x))})[1:5],150),times,pch=16,
      main="RF",xlab="k",ylab="Sample time (hrs)",
      sub=paste(signif(ct$estimate,3),signif(ct$p.value,3)))#
 abline(lm(times[-c(1,6)]~apply(cors_all_a,2,function(x){which(x==min(x))})[-c(1,6)]),col="black",lwd=1)
+
 ########Straight line, closest pass of trajectory plotted against true diff time
 cor.test(times[-c(1,6)],apply(ds,2,function(x){which(x==min(x))})[-c(1,6)])->ct2
 plot(apply(ds,2,function(x){which(x==min(x))}),times,pch=16,col=
